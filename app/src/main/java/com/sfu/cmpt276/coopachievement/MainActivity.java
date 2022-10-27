a9c99723e699dd6de58cc4994efc1fe12beb352b
bsa94 <banpreet_aulakh@sfu.ca>
2022-10-26 21:31:18 -0700

    Initial Commit

---
+++ app/src/main/java/com/sfu/cmpt276/coopachievement/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sfu.cmpt276.coopachievement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
