92e520b0a05b06949246185ea47c5580a3515847
bsa94 <banpreet_aulakh@sfu.ca>
2022-10-28 18:06:01 -0700

    Created basic GameHistory model class.

---
+++ app/src/main/java/com/sfu/cmpt276/coopachievement/model/GameHistory.java
@@ -0,0 +1,27 @@
+package com.sfu.cmpt276.coopachievement.model;
+
+import java.util.ArrayList;
+
+/*
+The GameHistory Class is responsible for storing all the instances of GamesPlayed per configured game. This facilitates instances of each config's
+game history being stored in the Singleton GameType class.
+ */
+public class GameHistory {
+
+    ArrayList<GamesPlayed> gamesPlayedList = new ArrayList<GamesPlayed>();
+
+    String configName;
+
+    public ArrayList<GamesPlayed> getGameHistory() {
+        return gamesPlayedList;
+    }
+
+    public void addPlayedGame(GamesPlayed game) {
+        gamesPlayedList.add(game);
+    }
+
+    public void removePlayedGame(int index) {
+        gamesPlayedList.remove(index);
+    }
+
+}
---
+++ app/src/main/java/com/sfu/cmpt276/coopachievement/model/GamesPlayed.java
@@ -0,0 +1,7 @@
+package com.sfu.cmpt276.coopachievement.model;
+
+/*
+THIS IS A STUB CLASS TO IMPLEMENT GameHistory. DELETE THIS AND REPLACE WITH ACTUAL CLASS DURING MERGE.
+ */
+public class GamesPlayed {
+}
