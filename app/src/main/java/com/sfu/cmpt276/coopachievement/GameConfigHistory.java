40003e65c1047f71a8acb3973e6995bccb821f46
bsa94 <banpreet_aulakh@sfu.ca>
2022-10-30 12:15:36 -0700

    Added Floating Action Button to GameHistory with stub code.

--- app/src/main/java/com/sfu/cmpt276/coopachievement/GameConfigHistory.java
+++ app/src/main/java/com/sfu/cmpt276/coopachievement/GameConfigHistory.java
@@ -2,7 +2,11 @@ package com.sfu.cmpt276.coopachievement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /*
 The GameConfigHistory Activity is responsible for displaying the instances of GamesPlayed in the GameHistory Class in a List Format. This is shown after a user
@@ -14,5 +18,15 @@ public class GameConfigHistory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_config_history);
+
+        FloatingActionButton fab = findViewById(R.id.add_new_game_btn);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GameConfigHistory.this, MainActivity.class); //STUB code for changing activities on FAB
+                GameConfigHistory.this.startActivity(intent);
+            }
+        });
     }
+
 }
--- app/src/main/java/com/sfu/cmpt276/coopachievement/MainActivity.java
+++ app/src/main/java/com/sfu/cmpt276/coopachievement/MainActivity.java
@@ -2,13 +2,28 @@ package com.sfu.cmpt276.coopachievement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+/*
+THIS IS A STUB MAIN ACTIVITY FOR TESTING ACTIVITY
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.game_history_btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameConfigHistory.class);
+                startActivity(intent);
+            }
+        });
     }
 }
