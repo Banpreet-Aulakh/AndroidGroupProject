ad332e6d9a772d5f7544b8dcb648aaa3a000926d
Preston <pcq@sfu.ca>
2022-11-30 02:05:55 -0800

    Created About Activity Page to have the names of the creators of the application.
    All links used is in a listView and clicking on the respective text will send the user to the page where the image, resource is found. Add any resource to the string resource xml. Put Link in string-array "links" and the corresponding name into "link name". Must have the same position/index

---
+++ app/src/main/java/com/sfu/cmpt276/coopachievement/AboutActivity.java
@@ -0,0 +1,47 @@
+package com.sfu.cmpt276.coopachievement;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class AboutActivity extends AppCompatActivity {
+    String[] urls;
+    String[] urlNames;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        ActionBar toolbar = getSupportActionBar();
+        toolbar.setTitle("About Page");
+
+        urls = getResources().getStringArray(R.array.links);
+        urlNames = getResources().getStringArray(R.array.link_name);
+
+        ListView resourceslist = findViewById(R.id.listViewResources);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                urlNames);
+        resourceslist.setAdapter(adapter);
+
+        resourceslist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent urlIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(urls[position]));
+                startActivity(urlIntent);
+            }
+        });
+
+    }
+}
