e1eb8020e9747e3f2f12954668e75061facfa706
Dante Umpherville-Choy <45216212+DanteUC@users.noreply.github.com>
2022-12-04 20:26:38 -0800

    Alert dialog is now normal sized instead of taking up the entire screen

--- app/src/main/java/com/sfu/cmpt276/coopachievement/PermissionsFragment.java
+++ app/src/main/java/com/sfu/cmpt276/coopachievement/PermissionsFragment.java
@@ -4,10 +4,15 @@ package com.sfu.cmpt276.coopachievement;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -33,4 +38,18 @@ public class PermissionsFragment extends AppCompatDialogFragment{
                .setPositiveButton(android.R.string.ok, listener)
                .create();
     }
+
+    @Override
+    public void onResume() {
+        //Source: https://gist.github.com/marc0x71/f571f4dfe328b662860cd4e80bd7f6af
+        Window window = getDialog().getWindow();
+        Point size = new Point();
+        Display display = window.getWindowManager().getDefaultDisplay();;
+        display.getSize(size);
+        window.setLayout((int) (size.x * 0.90), WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setGravity(Gravity.CENTER);
+
+        super.onResume();
+    }
+
 }
